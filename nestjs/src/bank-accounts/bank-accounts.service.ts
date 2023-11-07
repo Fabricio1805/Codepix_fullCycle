0a4510cba61b5842ad7f93b5a014d9a3cf3a5318
fabricio <fabricio.ss09@outlook.com>
2023-11-07 17:52:32 -0300

    refactor: CRUD bank-accounts

--- nestjs/src/bank-accounts/bank-accounts.service.ts
+++ nestjs/src/bank-accounts/bank-accounts.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@nestjs/common';
 import { CreateBankAccountDto } from './dto/create-bank-account.dto';
-import { UpdateBankAccountDto } from './dto/update-bank-account.dto';
 import { Repository } from 'typeorm';
 import { BankAccount } from './entities/bank-account.entity';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -16,19 +15,15 @@ export class BankAccountsService {
     return await this.bankAccountRepository.save(createBankAccountDto);
   }
 
-  findAll() {
-    return `This action returns all bankAccounts`;
+  async findAll() {
+    return await this.bankAccountRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} bankAccount`;
-  }
-
-  update(id: number, updateBankAccountDto: UpdateBankAccountDto) {
-    return `This action updates a #${id} bankAccount`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} bankAccount`;
+  async findOne(id: string) {
+    return await this.bankAccountRepository.findOneOrFail({
+      where: {
+        id,
+      },
+    });
   }
 }
